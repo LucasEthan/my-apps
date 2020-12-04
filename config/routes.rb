59f52822d0e288c49e42ad10bbdde64ecc1c0793
Lucas Ethan <asmrit1010@gmail.com>
2020-12-04 20:22:32 +0100

    Added links for the static pages and lorem ipsum placeholders

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/contact_us'
-  get 'static_pages/help'
+  root "static_pages#home"
+
+  get "about", to: "static_pages#about"
+  get "contact_us", to: "static_pages#contact_us"
+  get "help", to: "static_pages#help"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :military_time
 end
