2d9aab00788cb65f7d48f23ad96251704c9f7421
Lucas Ethan <asmrit1010@gmail.com>
2020-12-23 13:55:59 +0100

    Changed database to postgres

--- db/migrate/20201219072408_create_military_times.rb
+++ db/migrate/20201219072408_create_military_times.rb
@@ -1,7 +1,7 @@
 class CreateMilitaryTimes < ActiveRecord::Migration[6.0]
   def change
     create_table :military_times do |t|
-      t.integer, :military_time
+      t.integer :military_time
       t.string :standard_time
 
       t.timestamps
