ea774c808222b916fe83b77a51c4d8ca8e2447d0
Lucas Ethan <asmrit1010@gmail.com>
2021-01-20 15:12:52 +0100

    Made flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def display_flash_messages(flash)
+    content = ""
+    flash.each do |message_type, message|
+      content += tag.div(class: "alert alert-#{message_type}") do
+        tag.p(message)
+      end
+    end
+    content.html_safe
+  end
 end
