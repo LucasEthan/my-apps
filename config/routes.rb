1180873e3adefd9a820b27def45db0c8c3426776
Lucas Ethan <asmrit1010@gmail.com>
2021-01-23 09:31:20 +0100

    Added resources for craps

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'craps/show'
-  get 'craps/index'
-  get 'craps/destroy'
-  get 'craps/new'
-  get 'craps/create'
   root "static_pages#home"
 
   get "about", to: "static_pages#about"
@@ -11,6 +6,5 @@ Rails.application.routes.draw do
   get "help", to: "static_pages#help"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :military_times
-
   resources :craps
 end
