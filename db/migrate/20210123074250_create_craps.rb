95b82a66bda5121d8effa218986c60813fe7c74c
Lucas Ethan <asmrit1010@gmail.com>
2021-01-23 09:31:48 +0100

    Added craps attributes

--- db/migrate/20210123074250_create_craps.rb
+++ db/migrate/20210123074250_create_craps.rb
@@ -1,7 +1,8 @@
 class CreateCraps < ActiveRecord::Migration[6.0]
   def change
     create_table :craps do |t|
-
+      t.boolean :win, null: false
+      t.integer :rolls, array: true, null: false
       t.timestamps
     end
   end
