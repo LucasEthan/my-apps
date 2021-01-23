53fbb41743c36dc793b4e46da5a8f15895df80b8
Lucas Ethan <asmrit1010@gmail.com>
2021-01-23 09:56:35 +0100

    Updated seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Funds.delete_all
+
+Funds.create(amount: 10_000)
